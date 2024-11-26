af950e50ff30dba538fc7908350c2919ae794cb9
WiperZ <yura.4etin.03@gmail.com>
2024-11-26 20:01:12 +0800

    removed commented code

--- script.js
+++ script.js
@@ -1,72 +1,72 @@
-// // 1
-// let a = 10;
-// alert(a);
-// let a = 20;
-// alert(a);
+// 1
+let a = 10;
+alert(a);
+let b = 20;
+alert(b);
 
-// // 2
-// const iphone = 2007;
-// alert(iphone);
+// 2
+const iphone = 2007;
+alert(iphone);
 
-// // 3
-// const js = "Brendan Eich";
-// alert(js);
+// 3
+const js = "Brendan Eich";
+alert(js);
 
-// // 4
-// let oneNumber = 10;
-// let twoNumber = 2;
-// let sumNum = oneNumber + twoNumber;
-// let difNum = oneNumber - twoNumber;
-// let mpNum = oneNumber * twoNumber;
-// let divNum = oneNumber / twoNumber;
-// alert(sumNum);
-// alert(difNum);
-// alert(mpNum);
-// alert(divNum);
+// 4
+let oneNumber = 10;
+let twoNumBer = 2;
+let sumNum = oneNumber + twoNumBer;
+let difNum = oneNumber - twoNumBer;
+let mpNum = oneNumber * twoNumBer;
+let divNum = oneNumber / twoNumBer;
+alert(sumNum);
+alert(difNum);
+alert(mpNum);
+alert(divNum);
 
-// // 5
-// let degree = 2;
-// let result = degree ** 5;
-// alert(result);
+// 5
+let degree = 2;
+let result = degree ** 5;
+alert(result);
 
-// // 6
-// let remainderOne = 9;
-// let remainderTwo = 2;
-// let resRemainder = remainderOne % remainderTwo;
-// alert(resRemainder);
+// 6
+let remainderOne = 9;
+let remainderTwo = 2;
+let resRemainder = remainderOne % remainderTwo;
+alert(resRemainder);
 
-// // 7
-// let num = 1;
-// num += 5;
-// num -= 3;
-// num *= 7;
-// num /= 3;
-// num ++;
-// num --;
-// alert(num);
+// 7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num ++;
+num --;
+alert(num);
 
-// // 8 
-// let age = prompt("Сколько вам лет?");
-// alert(age);
+// 8 
+let age = prompt("Сколько вам лет?");
+alert(age);
 
-// // 9 
-// const user = {
-//     nAme: "Yurii",
-//     age: 21,
-//     isAdmin: true
-// }
+// 9 
+const user = {
+    nAme: "Yurii",
+    age: 21,
+    isAdmin: true
+}
 
-// // 10 
-// let userName = prompt("Ваше имя: ");
-// alert(`Привет, ${userName}`)
+// 10 
+let userName = prompt("Ваше имя: ");
+alert(`Привет, ${userName}`)
 
-// // 11
-// let numBer = Number(prompt("Загадайте любое число: "));
-// let twoNumber = numBer * 2;
-// alert(twoNumber);
-// let threeNumber = twoNumber + 10;
-// alert(threeNumber);
-// let fourNumber = threeNumber / 2;
-// alert(fourNumber);
-// let fiveNumber = fourNumber - numBer;
-// alert(fiveNumber);
+// 11
+let numBer = Number(prompt("Загадайте любое число: "));
+let twoNumber = numBer * 2;
+alert(twoNumber);
+let threeNumber = twoNumber + 10;
+alert(threeNumber);
+let fourNumber = threeNumber / 2;
+alert(fourNumber);
+let fiveNumber = fourNumber - numBer;
+alert(fiveNumber);
